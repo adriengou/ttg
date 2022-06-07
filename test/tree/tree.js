2f6b285a6415ee47516417ca59cc3c2a6c243c37
Adrien Gouacide <adrien.gouacide@mail.com>
2022-06-08 00:58:53 +0200

    commit

--- test/tree/tree.js
+++ test/tree/tree.js
@@ -51,25 +51,40 @@ function numberOfHalvings(n) {
     n = n / 2;
     count = count + 1;
   }
-
-  return count + 1;
+  //
+  return count;
 }
 
 function createTournament(list) {
   let tournament = [];
+
   let numberOfLevels = numberOfHalvings(list.length);
+  let numberOfMatches = list.length / 4;
 
   //create first level
   let level = createRandomMatches(list);
-  tournament.push(level);
+  tournament[0] = level;
 
-  //set random players for the first level
+  //create empty levels
+  for (let levelIndex = 1; levelIndex < numberOfLevels; levelIndex++) {
+    tournament.push([]);
+    tournament[levelIndex] = createEmptyMatches(numberOfMatches);
+    numberOfMatches = numberOfMatches / 2;
+  }
 
   return tournament;
 }
 
 function setWinner(tournament, matchLevel, matchIndex, player) {
   //set the winner in the match
+
+  if (
+    tournament[matchLevel][matchIndex].winner ||
+    !tournament[matchLevel][matchIndex].player1 ||
+    !tournament[matchLevel][matchIndex].player2
+  ) {
+    return false;
+  }
 
   switch (player) {
     case 1:
@@ -85,60 +100,115 @@ function setWinner(tournament, matchLevel, matchIndex, player) {
 
   let winner = tournament[matchLevel][matchIndex].winner;
   let newLevel = matchLevel + 1;
+  let newMatch = Math.floor(matchIndex / 2);
 
-  //check if level exists
-  if (tournament[newLevel]) {
-    tournament[newLevel].every((element) => {
-      if (element.player1 === "") {
-        element.player1 = winner;
-        return false;
-      } else if (element.player2 === "") {
-        element.player2 = winner;
-        return false;
-      }
-
-      return true;
-    });
-  } else {
-    //create level and add the match with a player in it
-    tournament[newLevel] = [];
-    let match = createMatch();
-    match.player1 = winner;
-    tournament[newLevel].push(match);
+  if (newLevel <= tournament.length - 1) {
+    if (matchIndex % 2 === 0) {
+      tournament[newLevel][newMatch].player1 = winner;
+    } else {
+      tournament[newLevel][newMatch].player2 = winner;
+    }
   }
 }
 
 function updateDom(tournament) {
-  let playerElement = document.querySelectorAll("p");
-  console.log(playerElement);
-  let arr = [];
+  for (let levelIndex = 0; levelIndex < tournament.length; levelIndex++) {
+    let level = tournament[levelIndex];
+    for (let matchIndex = 0; matchIndex < level.length; matchIndex++) {
+      let match = level[matchIndex];
 
-  for (row of tournament) {
-    for (e of row) {
-      arr.push(e);
+      let player1 = document.querySelector(`.l${levelIndex}.m${matchIndex}.p1`);
+
+      let player2 = document.querySelector(`.l${levelIndex}.m${matchIndex}.p2`);
+
+      if (match.player1) {
+        player1.textContent = match.player1;
+      }
+
+      if (match.player2) {
+        player2.textContent = match.player2;
+      }
     }
   }
 
-  let playerIndex = 0;
+  let winner = document.querySelector(".winner");
+  let level = tournament[tournament.length - 1];
+  winner.textContent = level[level.length - 1].winner;
+}
 
-  for (let index = 0; index < arr.length; index++) {
-    if (arr[index].player1) {
-      playerElement[playerIndex].textContent = arr[index].player1;
-    }
-    playerIndex++;
+function generateLevelDom(index) {
+  let level = document.createElement("div");
+  level.classList.add("level", `l${index}`);
+  return level;
+}
 
-    if (arr[index].player2) {
-      playerElement[playerIndex].textContent = arr[index].player2;
+function generateMatchDom(levelIndex, matchIndex) {
+  let match = document.createElement("div");
+  match.classList.add("match", `l${levelIndex}`, `m${matchIndex}`);
+  return match;
+}
+
+function generatePlayerDom(levelIndex, matchIndex, playerIndex, playerName) {
+  let player = document.createElement("p");
+
+  player.classList.add(
+    "player",
+    `l${levelIndex}`,
+    `m${matchIndex}`,
+    `p${playerIndex}`
+  );
+
+  player.textContent = playerName;
+
+  return player;
+}
+
+function generateDom(tournament) {
+  let body = document.querySelector("body");
+
+  for (let levelIndex = 0; levelIndex < tournament.length; levelIndex++) {
+    let level = tournament[levelIndex];
+    let levelDom = generateLevelDom(levelIndex);
+    body.appendChild(levelDom);
+
+    for (let matchIndex = 0; matchIndex < level.length; matchIndex++) {
+      let match = level[matchIndex];
+      let matchDom = generateMatchDom(levelIndex, matchIndex);
+      levelDom.appendChild(matchDom);
+
+      let player = "";
+
+      if (match.player1) {
+        player = match.player1;
+      }
+
+      let player1Dom = generatePlayerDom(levelIndex, matchIndex, 1, player);
+
+      matchDom.appendChild(player1Dom);
+
+      player = "";
+
+      if (match.player2) {
+        player = match.player2;
+      }
+
+      let player2Dom = generatePlayerDom(levelIndex, matchIndex, 2, player);
+
+      matchDom.appendChild(player2Dom);
     }
-    playerIndex++;
   }
+
+  // add a p for the winner
+  let winner = document.createElement("p");
+  winner.classList.add("player", "winner");
+  body.appendChild(winner);
 }
 
 const playerList = [
   "Adrien GOUACIDE",
   "Nicolas DUCHÊNE",
   "Stéphanie CHARY",
-  "NathanTEISSIER",
+  "Nathan TEISSIER",
   "Matteo FRA",
   "Imane QAJJOU",
   "Enzo MARTINEZ",
@@ -157,22 +227,33 @@ function test() {
   //create a tournament
   let tournament = createTournament(playerList);
 
-  console.log("tournament");
-  console.log(tournament);
-
   setWinner(tournament, 0, 0, 1);
-  console.log("tournament");
-  console.log(tournament);
 
   setWinner(tournament, 0, 1, 2);
-  console.log("tournament");
-  console.log(tournament);
 
   setWinner(tournament, 1, 0, 1);
-  console.log("tournament");
-  console.log(tournament);
 
   updateDom(tournament);
 }
 
-test();
+function testDom() {
+  let tournament = createTournament(playerList);
+
+  generateDom(tournament);
+
+  window.addEventListener("click", (event) => {
+    let player = event.target;
+    if (player.classList[1] === "winner") {
+      return true;
+    }
+
+    let levelIndex = parseInt(player.classList[1].replace("l", ""));
+    let matchIndex = parseInt(player.classList[2].replace("m", ""));
+    let playerIndex = parseInt(player.classList[3].replace("p", ""));
+
+    setWinner(tournament, levelIndex, matchIndex, playerIndex);
+    updateDom(tournament);
+  });
+}
+
+testDom();
