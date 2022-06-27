7ced55d6ac941cca565468f13e4cfe4abc821842
Adrien Gouacide <adrien.gouacide@mail.com>
2022-06-28 00:31:48 +0200

    commit

--- test/dragAndDrop/drag_and_drop.js
+++ test/dragAndDrop/drag_and_drop.js
@@ -1,14 +1,35 @@
 //config
 const elemSelector = ".square";
 const groupSelector = ".dragndrop";
-const styleToChange = "backgroundColor";
+
+//dom elements
+const group = document.querySelector(groupSelector);
+const elements = document.querySelectorAll(elemSelector);
+let squares = [];
+for (let index = 0; index < elements.length; index++) {
+  squares.push({
+    dom: elements[index],
+    x: elements[index].getBoundingClientRect().x,
+    y: elements[index].getBoundingClientRect().y,
+    start: elements[index].getBoundingClientRect(),
+  });
+}
+
+//variables
+let selectedElement = false;
+let swappingElement = false;
+let mouseX;
+let mouseY;
 
 //function to check if two element overlap
 function elementsOverlap(el1, el2) {
-  const domRect1 = el1.dom.getBoundingClientRect();
-  let domRect2;
+  const domRect1 = el1.getBoundingClientRect
+    ? el1.getBoundingClientRect()
+    : el1;
 
-  domRect2 = el2.startRect;
+  const domRect2 = el2.getBoundingClientRect
+    ? el2.getBoundingClientRect()
+    : el2;
 
   return !(
     domRect1.top > domRect2.bottom ||
@@ -18,47 +39,85 @@ function elementsOverlap(el1, el2) {
   );
 }
 
-//Create the array of objects
-let elemList = [];
+//function to move a dom element from its origin
+function moveElement(element, x, y, drag = false) {
+  let deltaX;
+  let deltaY;
+  if (drag) {
+    deltaX = mouseX - element.x;
+    deltaY = mouseY - element.y;
+  } else {
+    deltaX = 0;
+    deltaY = 0;
+  }
 
-//Currently selected element
-let selectedElem;
-let switchedElem;
+  let vectorX = x - element.x - deltaX;
+  let vectorY = y - element.y - deltaY;
 
-//Create object in the array
-elemList = document.querySelectorAll(elemSelector);
-
-//Adding the events
-for (const elem of elemList) {
-  elem.addEventListener("mousedown", function (e) {
-    selectedElem = elem;
-  });
+  element.dom.style.transform = `translate(${vectorX}px, ${vectorY}px)`;
 }
 
-//mousup event
-document.addEventListener("mouseup", function (e) {
-  selectedElem.style.zIndex = 0;
-  selectedElem.style.transform = `translate(0px, 0px)`;
-  selectedElem = false;
-});
+function swapElement(el1, el2) {
+  let copy = el1.textContent;
+  el1.textContent = el2.textContent;
+  el2.textContent = copy;
+}
 
-//Mouse event
-let mouseX;
-let mouseY;
-
-const groupElem = document.querySelector(groupSelector);
 document.addEventListener("mousemove", function (e) {
-  mouseX = e.screenX;
-  mouseY = e.screenY;
+  if (selectedElement !== false) {
+    // console.log(swappingElement);
+    moveElement(squares[selectedElement], e.clientX, e.clientY, true);
 
-  if (selectedElem) {
-    let domRect = selectedElem.getBoundingClientRect();
-    let vX, vY;
+    for (let index = 0; index < squares.length; index++) {
+      if (index !== selectedElement) {
+        console.log(
+          elementsOverlap(squares[selectedElement].dom, squares[index].start)
+        );
+        if (
+          elementsOverlap(squares[selectedElement].dom, squares[index].start)
+        ) {
+          if (swappingElement) {
+            squares[swappingElement].dom.style.transform =
+              "translate(0px, 0px)";
+          }
 
-    vX = mouseX - domRect.x;
-    vY = mouseY - domRect.y;
+          swappingElement = index;
 
-    selectedElem.style.transform = `translate(${vX}px, ${vY}px)`;
-    selectedElem.style.zIndex = 100;
+          moveElement(
+            squares[swappingElement],
+            squares[selectedElement].x,
+            squares[selectedElement].y
+          );
+        } else if (index === swappingElement) {
+          squares[swappingElement].dom.style.transform = "translate(0px, 0px)";
+          swappingElement = false;
+        }
+      }
+    }
   }
 });
+
+document.addEventListener("mouseup", function (e) {
+  // console.log(swappingElement);
+  if (selectedElement !== false && swappingElement !== false) {
+    let e1 = selectedElement;
+    let e2 = swappingElement;
+
+    for (const element of squares) {
+      console.log("test");
+      element.dom.style.transform = "translate(0px, 0px)";
+    }
+    swapElement(squares[e1].dom, squares[e2].dom);
+  } else {
+    squares[selectedElement].dom.style.transform = "translate(0px, 0px)";
+  }
+  selectedElement = false;
+});
+
+for (let index = 0; index < squares.length; index++) {
+  squares[index].dom.addEventListener("mousedown", function (e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+    selectedElement = index;
+  });
+}
